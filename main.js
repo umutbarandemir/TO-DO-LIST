88f89b008b8b70a4c4ebc4a566375d605f785d85
umutbarandemir <baran292907@hotmail.com>
2025-01-07 18:55:57 +0300

    LocalStorage Added,EventListener Added, BaseProjectDone

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
  
-    function addTast(){
+    function addTask(){
     if(inputBox.value === ""){
         alert("Enter a task !!!!");
     } else {
@@ -13,4 +13,25 @@ const listContainer = document.getElementById("list-container");
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveData();
 }
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI" ){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName ==="SPAN"){
+         e.target.parentElement.remove();
+         saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
